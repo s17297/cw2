bd26dbf8adf3984102a96cfca3e3a6bf0099e8a0
Maciek Niedziński <macius@MacBook-Pro-Maciek.local>
2020-03-14 16:17:29 +0100

    wczytywanie do listy

---
+++ cw2/cw2/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace cw2.Models
+{
+    public class Student
+    {
+        [XmlAttribute(AttributeName = "eska")] public string eska { get; set; }
+        [XmlElement(ElementName = "fName")] public string imie { get; set; }
+        [XmlElement(ElementName = "lName")] public string nazwisko { get; set; }
+        [XmlElement(ElementName = "email")] public string email { get; set; }
+        [XmlElement(ElementName = "dOfBirth")] public string dataUrodzenia { get; set; }
+        [XmlElement(ElementName = "mName")] public string imieMatki { get; set; }
+        [XmlElement(ElementName = "fathersName")] public string imieOjca { get; set; }
+        [XmlElement(ElementName = "studies")] public string trybStudiow { get; set; }
+        [XmlElement(ElementName = "degreeCourse")] public string kierunek { get; set; }
+
+    }
+}
--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -13,32 +13,47 @@ namespace cw2
             string path = @"Dane/dane.csv";
             Console.WriteLine("Hello World!");
 
+            var list = new List<Student>();
             //wczytywanie
+            int licz = 0;
             var fi = new FileInfo(path);
             using (var stream = new StreamReader(fi.OpenRead()))
             {
                 string line = null;
                 while ((line = stream.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    string[] kolumny = line.Split(',');
+                    if (kolumny.Length == 9) {
+                        licz++;
+                    }
+                    //Console.WriteLine(line);
+                    var st = new Student
+                    {
+                        imie = kolumny[0],
+                        nazwisko = kolumny[1],
+                        email = kolumny[6],
+                        dataUrodzenia = kolumny[5],
+                        imieMatki = kolumny[7],
+                        imieOjca = kolumny[8],
+                        trybStudiow = kolumny[3],
+                        kierunek = kolumny[2],
+                        eska = "s" + kolumny[4]
+                    };
+                    list.Add(st);
+                    Console.WriteLine(kolumny[8]);
                 }
+                Console.WriteLine(licz);
             }
             //stream.Dispose
 
             //xml
-            var list = new List<Student>();
-            var st = new Student
-            {
-                imie = "jan",
-                nazwisko = "kowalski",
-                email = "d@onet.pl"
 
-            };
-            list.Add(st);
+
             FileStream writer = new FileStream(@"data.xml", FileMode.Create);
             XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
             serializer.Serialize(writer, list);
-                writer.Dispose();
+            writer.Dispose();
+        
         }
     }
 }
